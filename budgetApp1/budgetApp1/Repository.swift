bb90e6751971563ac6c14971e8f60b7451566946
Marcus Nkadimeng <u19182504@tuks.co.za>
2024-04-14 00:42:22 +0200

    Transactions functionality
    Created a getTransactions() function

--- budgetApp1/budgetApp1/Repository.swift
+++ budgetApp1/budgetApp1/Repository.swift
@@ -9,6 +9,7 @@ import Foundation
 
 typealias BudgetsResult = (Result<BudgetData, APIError>) -> Void
 typealias AccountResult = (Result<AccountData, APIError>) -> Void
+typealias TransactionResult = (Result<TransactionData, APIError>) -> Void
 
 protocol BudgetRepositoryType: AnyObject {
     func getBudgets(completion: @escaping(BudgetsResult))
@@ -16,6 +17,10 @@ protocol BudgetRepositoryType: AnyObject {
 
 protocol AccountRepositoryType: AnyObject {
     func getAccounts(completion: @escaping(AccountResult))
+}
+
+protocol TransactionRepositoryType: AnyObject {
+    func getTransactions(completion: @escaping(TransactionResult))
 }
 
 class BudgetRepository: BudgetRepositoryType {
@@ -28,5 +33,11 @@ class BudgetRepository: BudgetRepositoryType {
 class AccountRepository: AccountRepositoryType {
     func getAccounts(completion: @escaping (AccountResult)) {
         APIHandler.request(endpoint: "https://api.ynab.com/v1/budgets/2b0c25e3-0d5e-411d-a019-2522faa22c2b/accounts?access_token=t_Ikm7Q2U94be-lmSn4blgh1sjIEf1dMF7Tv8jguvpU", method: .GET, completion: completion)
+    }
+}
+
+class TransactionRepository: TransactionRepositoryType {
+    func getTransactions(completion: @escaping (TransactionResult)) {
+        APIHandler.request(endpoint: "https://api.ynab.com/v1/budgets/2b0c25e3-0d5e-411d-a019-2522faa22c2b/accounts/0515719a-f641-422b-8341-7073d5358dbe/transactions?access_token=t_Ikm7Q2U94be-lmSn4blgh1sjIEf1dMF7Tv8jguvpU", method: .GET, completion: completion)
     }
 }
---
+++ budgetApp1/budgetApp1/TransactionViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  TransactionViewModel.swift
+//  budgetApp1
+//
+//  Created by Lehlohonolo Nkadimeng on 2024/04/14.
+//
+
+import Foundation
+
